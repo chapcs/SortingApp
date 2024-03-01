e51fd28bba8ffe456d099c558d3d4f78ac5e0fbc
chapcs <benchapman003@gmail.com>
2024-03-01 07:35:36 -0500

    Added comment

--- ClientApp/src/algorithm/bubblesort.service.ts
+++ ClientApp/src/algorithm/bubblesort.service.ts
@@ -1,11 +1,54 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BubblesortService {
+  array = [];
+  toDispatch = [];
+  sorted = false;
+  round = 0;
+  dispatchArray = new Subject<any[]>();
 
   constructor() { }
+
+  bubbleSort(inputArray: number[]) {
+    this.array = [...inputArray];
+    while (!this.sorted) {
+      this.sorted = true;
+      for (let i = 0; i < this.array.length - 1 - this.round; i++) {
+        this.toDispatch.push([i, i + 1]);
+        if (this.array[i] > this.array[i + 1]) {
+          this.toDispatch.push([i, i + 1, true]);
+          let temp = this.array[i];
+          this.array[i] = this.array[i + 1];
+          this.array[i + 1] = temp;
+          this.sorted = false;
+          this.toDispatch.push(this.array.slice(0));
+          this.toDispatch.push([]);
+        }
+      }
+      this.toDispatch.push([true, this.array.length - 1 - this.round]);
+      this.round++;
+    }
+
+    this.handleDispatch();
+    return this.array;
+  }
+
+  handleDispatch() {
+    if (!this.toDispatch.length) {
+      setTimeout(() => {
+        this.dispatchArray.next(this.array.map((num, index) => index));
+      }, 900);
+      return;
+    }
+    this.dispatchArray.next(this.toDispatch.shift());
+    setTimeout(() => {
+      this.handleDispatch();
+    }, 50); // speed is set to 50ms for demonstration
+  }
 }
 
 /* BELOW IS THE COPY OVER LISTED, THIS NEEDS INTEGRATED INTO THE CREATED SERVICE.
